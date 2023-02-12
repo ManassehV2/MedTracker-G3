8ea68f5be1e9e4c641e145a3bffa0a156f1a2d9b
NaolAklilu <naolaklilu12@gmail.com>
2023-02-12 10:14:02 +0300

    Add IDiagnosesService

---
+++ src/MedAdvisor.Services.Okta/Interfaces/IDiagnosesService.cs
@@ -0,0 +1,11 @@
+﻿
+using MedAdvisor.Models;
+namespace MedAdvisor.Services.Okta.Interfaces
+{
+    public interface IDiagnosesService
+    {
+        Task<Diagnoses> GetDiagnoses(Guid id);
+        Task<User> AddDiagnoses(Guid id);
+        void DeleteDiagnoses(Guid id);
+    }
+}
