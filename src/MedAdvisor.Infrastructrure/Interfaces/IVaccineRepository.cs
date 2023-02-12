546b328ad61d4f636496ab7a7ae24b37fd26bc05
@Brownoak <helanii93@gmail.com>
2023-02-12 10:48:36 +0300

    added IVaccineRepository

---
+++ src/MedAdvisor.Infrastructrure/Interfaces/IVaccineRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using MedAdvisor.Models;
+
+namespace MedAdvisor.Infrastructrure.Interfaces
+{
+    public interface IVaccineRepository
+    {
+        Task<IEnumerable<Vaccine>> SearchVaccines(string name);
+        Task<User> AddVaccineAsync(User user, Vaccine vaccine);
+        Task<User> DeleteVaccineAsync(User user, Vaccine vaccine);
+        Task<Vaccine> GetVaccine(Guid id);
+    }
+}
