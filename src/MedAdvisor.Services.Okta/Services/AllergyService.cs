12ce758c65b15ac6df6b19805a3fde820f27b713
NaolAklilu <naolaklilu12@gmail.com>
2023-02-12 10:03:22 +0300

    Add AllergyService

---
+++ src/MedAdvisor.Services.Okta/Services/AllergyService.cs
@@ -0,0 +1,27 @@
+﻿using MedAdvisor.DataAccess.MySql.Repositories;
+using MedAdvisor.Models;
+using MedAdvisor.Services.Okta.Interfaces;
+
+namespace MedAdvisor.Services.Okta.Services
+{
+    public class AllergyService : IAllergyService
+    {
+        private readonly IAllergyRepository _allergyRepository;
+        public AllergyService(IAllergyRepository allergyRepository )
+        {
+        _allergyRepository = allergyRepository;
+        }
+        public async Task<Allergy> Get(Guid id)
+        {
+            var allergy = await _allergyRepository.GetAllergy(id);
+            return allergy;
+        }
+        public Task<User> Add(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+
+    }
+}
