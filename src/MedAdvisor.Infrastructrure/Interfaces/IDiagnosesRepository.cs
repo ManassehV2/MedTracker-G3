c93bf8dd98df21476eb845265be28c3fcfda5384
@Brownoak <helanii93@gmail.com>
2023-02-12 10:44:22 +0300

    added IDiagnosesRepository

---
+++ src/MedAdvisor.Infrastructrure/Interfaces/IDiagnosesRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using MedAdvisor.Models;
+
+namespace MedAdvisor.Infrastructrure.Interfaces
+{
+    public interface IDiagnosesRepository
+    {
+        Task<IEnumerable<Diagnoses>> SearchDiagnoses(string name);
+        Task<User> AddDiagnosesAsync(User user, Diagnoses diagnoses);
+        Task<User> DeleteDiagnosesAsync(User user, Diagnoses diagnoses);
+        Task<Diagnoses> GetDiagnoses(Guid id);
+    }
+}
