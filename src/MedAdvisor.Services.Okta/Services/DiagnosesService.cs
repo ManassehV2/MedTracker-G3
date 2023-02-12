1d054851dafe457c4763757d2f51d603c3a4193b
NaolAklilu <naolaklilu12@gmail.com>
2023-02-12 10:06:27 +0300

    Add DiagnosesService

---
+++ src/MedAdvisor.Services.Okta/Services/DiagnosesService.cs
@@ -0,0 +1,31 @@
+﻿
+using MedAdvisor.Infrastructrure.Interfaces;
+using MedAdvisor.Services.Okta.Interfaces;
+using MedAdvisor.Models;
+
+namespace MedAdvisor.Services.Okta.Services
+{
+    public class DiagnosesService : IDiagnosesService
+    {
+        private readonly IDiagnosesRepository _diagnosesRepository;
+        public DiagnosesService(IDiagnosesRepository digrepository)
+        {
+            _diagnosesRepository = digrepository;
+        }
+        public Task<User> AddDiagnoses(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteDiagnoses(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Diagnoses> GetDiagnoses(Guid id)
+        {
+            var diagnoses = await _diagnosesRepository.GetDiagnoses(id);
+            return diagnoses;
+        }
+    }
+}
