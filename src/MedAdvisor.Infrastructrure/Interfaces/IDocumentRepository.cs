0d1aa4a95c39cf3557eae829ea7170a33a541b2f
@Brownoak <helanii93@gmail.com>
2023-02-12 10:45:57 +0300

    added IDocumentRepository

---
+++ src/MedAdvisor.Infrastructrure/Interfaces/IDocumentRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using MedAdvisor.Models;
+
+namespace MedAdvisor.Infrastructrure.Interfaces
+{
+    public interface IDocumentRepository
+    {
+        Task<Document> AddDocumentAsync(User user, Document document);
+        Task DeleteDocumentAsync( Document document);
+        Task<Document> GetDocumentById(Guid id);
+        Task<Document> UdpdateDocumentAsync(Document document);
+    }
+}
