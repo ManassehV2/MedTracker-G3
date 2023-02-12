c397f97a2fc02fbb5afbf3ecb59e2735d9a011c7
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-02-12 17:12:11 +0300

    adding custome responses

---
+++ src/MedAdvisor.Api/Responses/LoginResponse.cs
@@ -0,0 +1,17 @@
+﻿using MedAdvisor.Models;
+
+namespace MedAdvisor.Api.Responses
+{
+    public class LoginResponse
+    {
+        public string Message { get; set; }
+        public User user { get; set; }
+        public string token { get; set; }
+        public LoginResponse(string message, User new_user,string tokens)
+        {
+            Message = message;
+            user = new_user;
+            token = tokens;
+        }
+    }
+}
---
+++ src/MedAdvisor.Api/Responses/RegistrationResponse.cs
@@ -0,0 +1,15 @@
+﻿using MedAdvisor.Models;
+
+namespace MedAdvisor.Api.Responses
+{
+    public class RegistrationResponse
+    {
+        public string Message { get; set; }
+        public User user { get; set; }
+        public RegistrationResponse(string message, User new_user)
+        {
+            Message = message;
+            user = new_user;
+        }
+    }
+}
