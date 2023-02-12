661e35afa6009e91883234c00476290fdd59246b
NaolAklilu <naolaklilu12@gmail.com>
2023-02-12 10:13:00 +0300

    IAllergyService

---
+++ src/MedAdvisor.Services.Okta/Interfaces/IAllergyService.cs
@@ -0,0 +1,10 @@
+﻿using MedAdvisor.Models;
+
+namespace MedAdvisor.Services.Okta.Interfaces
+{
+    public interface IAllergyService
+    {
+        Task<Allergy> Get(Guid id);
+        Task<User> Add(Guid id);
+    }
+}
