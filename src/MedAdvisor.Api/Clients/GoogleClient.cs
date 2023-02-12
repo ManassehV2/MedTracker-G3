3c98b3d9c42b212628f2251aad19afdca3916248
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-02-12 15:22:47 +0300

    adding clinet

---
+++ src/MedAdvisor.Api/Clients/GoogleClient.cs
@@ -0,0 +1,31 @@
+﻿
+using Google.Apis.Auth;
+using MedAdvisor.Services.Okta.Interfaces;
+using Microsoft.Extensions.Options;
+
+
+namespace MedAdvisor.Api.Clients
+{
+    public class GoogleClient
+    {
+        private readonly GoogleAuthSettings _googleAuthSettings;
+        public GoogleClient(
+            IOptionsSnapshot<GoogleAuthSettings> googleAuthSettings
+            )
+        {
+            _googleAuthSettings = googleAuthSettings.Value;
+
+        }
+
+        public async Task<GoogleJsonWebSignature.Payload> VerifyGoogleToken(string token)
+        {
+            var settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = new List<string>() { _googleAuthSettings.ClientId }
+            };
+            var payload = await GoogleJsonWebSignature.ValidateAsync(token, settings);
+            return payload;
+        }
+
+    }
+}
