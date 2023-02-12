5d21da69a585441f800536ca4cd19d7e6ba51c36
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-02-12 15:15:24 +0300

    adding password reset

---
+++ src/MedAdvisor.Commons/Email/EmailHelper.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Net;
+using System.Net.Mail;
+using MedAdvisor.Commons.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+
+namespace rentX.Common.Email
+{
+    public  class EmailHelper
+    {
+        private readonly IConfiguration _configuration;
+        private readonly MailSettings _mailSettings;
+
+        public  EmailHelper(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+        public async Task<bool> SendEmail(string userEmail)
+        {
+            var From = _configuration.GetSection("EmailConfiguration:From").Value;
+            var UserName = _configuration.GetSection("EmailConfiguration:Username").Value;
+            var Password = _configuration.GetSection("EmailConfiguration:Password").Value;
+
+          
+            MailAddress to = new MailAddress(From);
+            MailAddress from = new MailAddress(userEmail);
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = "Good morning, Charles";
+            message.Body = "Charles, Long time no talk. Would you be up for lunch in Soho on Monday? I'm paying.;";
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(From, Password),
+                EnableSsl = true
+            };
+            try
+            {
+                 client.Send(message);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                var a = "test";
+            }
+            return false;
+        }
+
+    }
+}
---
+++ src/MedAdvisor.Commons/Email/MailSettings.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace MedAdvisor.Commons.Email
+{
+  public class MailSettings
+    {
+        public string From { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
