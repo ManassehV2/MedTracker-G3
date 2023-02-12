2305ad4aa21a8c9985bc21dddced2911485cf243
@Brownoak <helanii93@gmail.com>
2023-02-12 10:47:06 +0300

    added IProfileRepository

---
+++ src/MedAdvisor.Infrastructrure/Interfaces/IProfileRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using MedAdvisor.Models;
+
+namespace MedAdvisor.Infrastructrure.Interfaces
+{
+    public interface IProfileRepository
+    {
+        Task<UserProfile> AddProfileAsync(UserProfile profile,User user);
+        Task<UserProfile> UdpdateProfile(UserProfile profile);
+        Task<UserProfile> GetProfile(Guid userId);
+
+    }
+}
