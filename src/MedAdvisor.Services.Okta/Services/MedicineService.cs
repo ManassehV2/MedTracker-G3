993d4b0b52c10ba87c0d2517e40cc640d7cb6a91
NaolAklilu <naolaklilu12@gmail.com>
2023-02-12 10:08:48 +0300

    Add MedicineService

---
+++ src/MedAdvisor.Services.Okta/Services/MedicineService.cs
@@ -0,0 +1,35 @@
+﻿
+
+using MedAdvisor.Infrastructrure.Interfaces;
+using MedAdvisor.Models;
+using MedAdvisor.Services.Okta.Interfaces;
+
+namespace MedAdvisor.Services.Okta.Services
+{
+    public class MedicineService : IMedicineService
+    {
+        private readonly ImedicineRepository _medicineRepository;
+
+        public MedicineService(ImedicineRepository medrepository)
+        {
+            _medicineRepository = medrepository;
+
+        }
+        public async Task<Medicine> GetMedicine(Guid id)
+        {
+            var medicine = await _medicineRepository.GetMedicine(id);
+            return medicine;
+        }
+        public Task<User> AddMedicine(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMedicine(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+       
+    }
+}
