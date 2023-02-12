be45605783dcd648ae1fdb0c07db5e5f408f1c4d
NaolAklilu <naolaklilu12@gmail.com>
2023-02-12 10:11:57 +0300

    Add VaccineService

---
+++ src/MedAdvisor.Services.Okta/Services/VaccineService.cs
@@ -0,0 +1,34 @@
+﻿
+using MedAdvisor.Infrastructrure.Interfaces;
+using MedAdvisor.Models;
+using MedAdvisor.Services.Okta.Interfaces;
+
+
+namespace MedAdvisor.Services.Okta.Services
+{
+    public class VaccineService : IVaccineService
+
+    {
+        private readonly IVaccineRepository _vaccineRepository;
+
+        public VaccineService(IVaccineRepository vaccineRepository)
+        {
+            _vaccineRepository = vaccineRepository;
+        }
+        public Task<User> AddVaccine(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteVaccine(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Vaccine> GetVaccine(Guid id)
+        {
+            var vaccine = await _vaccineRepository.GetVaccine(id);
+            return vaccine;
+        }
+    }
+}
