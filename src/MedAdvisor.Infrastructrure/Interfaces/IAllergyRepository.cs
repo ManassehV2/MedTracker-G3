066a5abee21df223888d354552b2f6dd2e92bc47
@Brownoak <helanii93@gmail.com>
2023-02-12 10:36:58 +0300

    add Iallergy repository

---
+++ src/MedAdvisor.Infrastructrure/Interfaces/IAllergyRepository.cs
@@ -0,0 +1,15 @@
+﻿
+
+using MedAdvisor.Models;
+
+namespace MedAdvisor.DataAccess.MySql.Repositories
+{
+    public interface  IAllergyRepository
+    {
+        Task<IEnumerable<Allergy>> SearchAllergyies(string N);
+        Task<User> AddAllergyAsync(User user, Allergy allergy);
+        Task<User> DeleteAllergyAsync(User user, Allergy allergy);
+
+        Task<Allergy> GetAllergy(Guid id);
+    }
+}
