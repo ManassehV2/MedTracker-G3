5c78b7ec1164e383c8b35ad426ed9f2e2e97dd7b
@Brownoak <helanii93@gmail.com>
2023-02-12 10:48:03 +0300

    added IUserRepository

---
+++ src/MedAdvisor.Infrastructrure/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using MedAdvisor.Models;
+namespace MedAdvisor.DataAccess.MySql.Repositories.Users
+{
+   public interface IUserRepository
+    {
+        Task<User> GetUserById(Guid id);
+        Task<User> GetUserByEmail(string email);
+        Task<User> AddUserAsync(User user);
+        Task<User> FetchUserData(string email);
+    }
+}
