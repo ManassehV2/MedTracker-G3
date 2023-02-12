b577c4a700749854fecf7473de17b1e7c123326f
NaolAklilu <naolaklilu12@gmail.com>
2023-02-12 10:10:01 +0300

    Add ProfileService

---
+++ src/MedAdvisor.Services.Okta/Services/ProfileService.cs
@@ -0,0 +1,56 @@
+﻿
+
+using MedAdvisor.Infrastructrure.Interfaces;
+using MedAdvisor.Models;
+using MedAdvisor.Services.Okta.Interfaces;
+
+namespace MedAdvisor.Services.Okta.Services
+{
+    public class ProfileService : IProfileService
+    {
+        private readonly IProfileRepository _profileRepository;
+
+        public ProfileService(IProfileRepository profileRepository)
+        {
+            _profileRepository = profileRepository;
+        }
+        public async Task<UserProfile> AddProfile(UserProfile profile,User user)
+        {
+            try
+            {
+                var newprofile = await _profileRepository.AddProfileAsync(profile,user);
+                return newprofile;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<UserProfile> GetProfile(Guid id)
+        {
+            try
+            {
+                var profile = await _profileRepository.GetProfile(id);
+                return profile;
+            }
+            catch (Exception ex )
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<UserProfile> updateProfile(UserProfile profile)
+        {
+            try
+            {
+                var updated = await _profileRepository.UdpdateProfile(profile);
+                return updated;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
