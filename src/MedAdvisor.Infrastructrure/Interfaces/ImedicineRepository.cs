672784a2afbc88cd0ab3e958e3066a7b2a4622ec
@Brownoak <helanii93@gmail.com>
2023-02-12 10:46:28 +0300

    added IMedicine Repository

---
+++ src/MedAdvisor.Infrastructrure/Interfaces/ImedicineRepository.cs
@@ -0,0 +1,14 @@
+﻿
+
+using MedAdvisor.Models;
+
+namespace MedAdvisor.Infrastructrure.Interfaces
+{
+    public interface ImedicineRepository
+    {
+        Task<IEnumerable<Medicine>> SearchMedicines(string name);
+        Task<User> AddMedicineAsync(User user, Medicine medicine);
+        Task<User> DeleteMedicineAsync(User user, Medicine allergy);
+        Task<Medicine> GetMedicine(Guid id);
+    }
+}
